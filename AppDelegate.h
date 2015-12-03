a01f8aa9f7d004af2c0d25d4df17cce63e828e82
JunPingWon <1436538028@qq.com>
2015-12-03 16:19:48 +0800

    UIStackView-Demo

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackviewTest
+//
+//  Created by JunpingWon on 15/12/2.
+//  Copyright © 2015年 JunpingWon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackviewTest
+//
+//  Created by JunpingWon on 15/12/2.
+//  Copyright © 2015年 JunpingWon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
